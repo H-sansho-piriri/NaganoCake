40ef51b94890d642700eccf42ea427efbd0760c2
Falcon8810 <hayato.sa9211@gmail.com>
2021-11-17 11:13:02 +0000

    [Add cart_products order_details]

--- db/migrate/20211117075635_create_order_details.rb
+++ db/migrate/20211117075635_create_order_details.rb
@@ -1,7 +1,12 @@
 class CreateOrderDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :order_details do |t|
-
+      t.integer :order_id
+      t.integer :product_id
+      t.integer :quantity
+      t.integer :making_status, default: 0
+      t.integer :price
+      
       t.timestamps
     end
   end
