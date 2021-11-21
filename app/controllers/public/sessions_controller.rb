f12782e5f1c8b875b1451f12429f1006cb932176
Falcon8810 <hayato.sa9211@gmail.com>
2021-11-21 05:11:24 +0000

    [Add]退会している場合の処理

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +25,18 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  protected
+  ##退会されているかを判断するメソッド
+  def customer_state
+    ##【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ##アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@Cusromer
+    ##【処理内容2】取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      redirect_to new_customer_registration_path
+    end
+  end
+    
 end
