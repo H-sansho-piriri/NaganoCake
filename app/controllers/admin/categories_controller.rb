450c034d5190b3c1db4cfbf036bf522f16f752c5
fyky <fuyu12dmb25ms@gmail.com>
2021-11-21 15:59:29 +0000

    [Add]categories.controller フォーム定義

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,30 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @category = Category.new
+    @categories = Category.all
+
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to admin_categories_path
   end
 
   def edit
+
   end
+
+  def update
+  end
+
+private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
