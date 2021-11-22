1e0abf30db84b25b93ae8d5ae969c42a25e2cc28
fyky <fuyu12dmb25ms@gmail.com>
2021-11-22 06:25:43 +0000

    [Fix]viewページ調整

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,4 +1,18 @@
 class Public::CartProductsController < ApplicationController
   def index
+    @cart_product =
+    @cart_products = CartProduct.all
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def destroy_all
   end
 end
