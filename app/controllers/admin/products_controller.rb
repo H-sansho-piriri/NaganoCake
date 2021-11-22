a9b043ac2621519d23b740c801e879692e4a962e
Taka-Adelaide <takapenang08@gmail.com>
2021-11-22 06:23:14 +0000

    boolean_error

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -20,10 +20,15 @@ class Admin::ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
-    @product.update(product_params)
-    redirect_to admin_product_path(product.id)
     @categories = Category.all
+    @product = Product.find(params[:id])
+    
+  end
+  
+  def update
+    product = Product.find(params[:id])
+    product.update(product_params)
+    redirect_to admin_product_path(product.id)
   end
 
   private
