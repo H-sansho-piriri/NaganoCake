5d6ae05158220269c6ddd19b56f77a5a1fc0177b
Taka-Adelaide <takapenang08@gmail.com>
2021-11-23 09:56:36 +0000

    [CREATE]Orders/new_view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,12 @@
 class Public::OrdersController < ApplicationController
   def new
-    
+    @order = Order.new
   end
 
   def confirm
+  end
+  
+  def create
   end
 
   def complete
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
+  belongs_to :custome
   enum payment_method: { credit_card: 0, transfer: 1}
 end
