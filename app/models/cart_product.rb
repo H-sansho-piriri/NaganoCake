67ba91885227f513704560428ec0024c3982e45f
fyky <fuyu12dmb25ms@gmail.com>
2021-11-23 14:53:28 +0000

    [Add]注文内容送信ボタン

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,4 +1,6 @@
 class CartProduct < ApplicationRecord
+  validates :quantity, numericality: { less_than_or_equal_to: 30, greater_than_or_equal_to: 1 }
+
   def subtotal
     product.add_tax_price * quantity
   end
