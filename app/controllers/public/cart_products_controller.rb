3660a060a93077b4d37be131f6117ced7a8db8c9
fyky <fuyu12dmb25ms@gmail.com>
2021-11-23 04:48:47 +0000

    [Add]カート情報を保存

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,10 +1,26 @@
 class Public::CartProductsController < ApplicationController
   def index
-    @cart_product =
     @cart_products = CartProduct.all
+
+    # @products = CartProduct.all
   end
 
   def create
+    # binding.pry
+    @cart_product = CartProduct.new(cart_product_params)
+    @cart_product.customer_id = current_customer.id
+    # if
+    @cart_product.save
+    redirect_to customers_cart_products_path
+    # else
+    # render:index
+    # end
+    # @cart_product.customer = current_customer
+    # @cart_product.product.id = params[:product_id]
+    # @task = Task.new(task_params)
+    # @cart_products = CartProduct.all
+    # @cart_product = CartProduct.find(cart_product_params[:product_id])
+    # @cart_product.save
   end
 
   def update
@@ -16,6 +32,7 @@ class Public::CartProductsController < ApplicationController
   def destroy_all
   end
 
+#product_idとquantityを取り出し格納するため
 private
 def cart_product_params
     params.require(:cart_product).permit(:product_id, :quantity)
