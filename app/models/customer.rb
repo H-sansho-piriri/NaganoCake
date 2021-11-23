e13c8a1ea57685c8a06a9e4cd110a59fa7b61fd5
Falcon8810 <hayato.sa9211@gmail.com>
2021-11-23 18:26:55 +0000

    [Fix]バリデーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,10 +6,11 @@ class Customer < ApplicationRecord
          
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :postal_code, presence: true
-  validates :phone_number, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\p{katakana}/ }
+  validates :first_name_kana, presence: true, format: { with: /\p{katakana}/ }
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+  validates :address, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
   
   def full_name
     last_name + first_name
