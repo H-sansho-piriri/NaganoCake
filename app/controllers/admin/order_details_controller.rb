428895259bd527559c9091f09732c5398793be99
Falcon8810 <hayato.sa9211@gmail.com>
2021-11-24 16:05:25 +0000

    [Add]

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,15 @@
 class Admin::OrderDetailsController < ApplicationController
+  def update
+    @order = Order.find(params[:id])
+    @order_detail = @order.order_details
+    @order_detail.update(order_details_params)
+    
+    redirect_to request.referer
+  end
+  
+  
+  private
+  def order_details_params
+    params.require(:order_detail).permit(:making_status)
+  end
 end
