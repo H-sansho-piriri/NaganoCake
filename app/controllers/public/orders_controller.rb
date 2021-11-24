b93496ca2221fe3b2e5740bd7f752b7959a5d893
Taka-Adelaide <takapenang08@gmail.com>
2021-11-24 09:49:00 +0000

    [CREATE]Orders/confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,8 +4,37 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @cart_products = CartProduct.all
+    @shipping_fee = 800
+    @total = 0
+
+    @cart_products.each do |cart_product|
+      @total += cart_product.subtotal
+    end
+
+    if params[:order][:address_number] == "1"
+      @order = Order.new(order_params)
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.full_name
+
+    elsif params[:order][:address_number] == "2"
+
+      @order = Order.new(order_params)
+      @address = Shipping.find(params[:order][:shipping_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+
+    elsif params[:order][:address_number] == "3"
+
+      @order = Order.new(order_params)
+      # @order.postal_code = @address.postal_code
+      # @order.address = @address.address
+      # @order.name = @address.name
+    end
   end
-  
+
   def create
   end
 
@@ -17,4 +46,10 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :custome
   enum payment_method: { credit_card: 0, transfer: 1}
+  def full_address
+    '〒' + postal_code + ' ' + address + ' ' + name
+  end
+
+  
+
 end
