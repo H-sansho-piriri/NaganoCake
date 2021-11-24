26250ec8571a9055af072fc4e9f00042c44dca60
Falcon8810 <hayato.sa9211@gmail.com>
2021-11-24 10:19:26 +0000

    [Add]admin注文履歴詳細途中,時間日本語化

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,17 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
     @orders = Order.all
   end
 
   def show
-    # @order = Order.find(params[:id])
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @total = 0
+    # これだと税抜？
+    @order_details.each do |product|
+      @total += product.price * product.quantity
+    end
   end
 end
