f46cbc3a2b9c675147d5206af5d81f746fe03e1e
Falcon8810 <hayato.sa9211@gmail.com>
2021-11-24 16:27:18 +0000

    [Add]making_status完成

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,6 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    @order = Order.find(params[:id])
-    @order_detail = @order.order_details
+    @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_details_params)
     
     redirect_to request.referer
