10ea97ba992a7a4be9358d21f8dbe8aba9e78c1c
Taka-Adelaide <takapenang08@gmail.com>
2021-11-26 05:31:30 +0000

    [自動連動作成]

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,7 +3,16 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_details_params)
+    @order = @order_detail.order
 
+
+    if @order_detail.making_status == "coming_soon"
+      @order_detail.order.update(status: "making_now")
+
+    elsif @order.order_details.where(making_status: "now_on_air").count == @order.order_details.count
+      @order_detail.order.update(status: "preparing_delivery")
+
+    end
 
     redirect_to request.referer
   end
