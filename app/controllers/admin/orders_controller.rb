dea6186f8ee0a631f01940a13386a22995a62b09
fyky <fuyu12dmb25ms@gmail.com>
2021-11-26 04:38:02 +0000

    [Add]自動変更ステータス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,6 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
     @orders = Order.all
   end
@@ -14,17 +14,23 @@ class Admin::OrdersController < ApplicationController
       @total += order_detail.product.add_tax_price * order_detail.quantity
     end
   end
-  
+
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+
+    if @order.status == "payment_cofirm"
+      @order.order_details.update(making_status: "wating")
+    end
+
     redirect_to request.referer
   end
-  
+
+
   private
   def order_params
     params.require(:order).permit(:status)
   end
-  
-  
+
+
 end
